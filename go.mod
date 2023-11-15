6ac5db48e9a53d6007ba28c40abea09ff1deae29
Petter Thorbjørnsen <petter.thorbjornsen@tv2.no>
2023-11-15 09:45:38 +0100

    Initial upload

---
+++ go.mod
@@ -0,0 +1,15 @@
+module github.com/pettertho/intervals-api
+
+go 1.21.1
+
+require github.com/gorilla/handlers v1.5.2
+
+require (
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+)
+
+require (
+	github.com/felixge/httpsnoop v1.0.3 // indirect
+	github.com/gorilla/mux v1.8.1
+)
---
+++ main.go
@@ -0,0 +1,129 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"sort"
+
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
+	log "github.com/sirupsen/logrus"
+)
+
+type Interval struct {
+	Start int
+	End   int
+}
+
+func handleAPI(w http.ResponseWriter, r *http.Request) {
+	log.Println("Received an API request")
+	var request struct {
+		Includes []Interval `json:"includes"`
+		Excludes []Interval `json:"excludes"`
+	}
+
+	err := json.NewDecoder(r.Body).Decode(&request)
+	if err != nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}
+	log.Info(request)
+	log.Info(request.Includes)
+	log.Info(request.Excludes)
+	output := processIntervals(request.Includes, request.Excludes)
+
+	response := struct {
+		Output []Interval `json:"output"`
+	}{
+		Output: output,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
+}
+
+func mergeIntervals(intervals []Interval) []Interval {
+	log.Println("Merging intervals")
+	merged := []Interval{}
+
+	sort.Slice(intervals, func(i, j int) bool {
+		return intervals[i].Start < intervals[j].Start
+	})
+
+	for _, interval := range intervals {
+		if len(merged) == 0 || merged[len(merged)-1].End < interval.Start {
+			merged = append(merged, interval)
+		} else {
+			merged[len(merged)-1].End = max(merged[len(merged)-1].End, interval.End)
+		}
+	}
+	return merged
+}
+
+func processIntervals(includes, excludes []Interval) []Interval {
+	result := []Interval{}
+
+	// Merge overlapping intervals in includes and excludes
+	includes = mergeIntervals(includes)
+	excludes = mergeIntervals(excludes)
+
+	// Process intervals
+	for _, include := range includes {
+		for _, exclude := range excludes {
+			// If exclude interval is within include interval
+			if exclude.Start > include.End || exclude.End < include.Start {
+				continue
+			} else {
+				// Adjust include interval based on exclude interval
+				if exclude.Start <= include.Start && exclude.End >= include.End {
+					// Exclude interval completely covers include interval
+					include = Interval{}
+					break
+				} else if exclude.Start <= include.Start {
+					// Exclude interval overlaps with the start of include interval
+					include.Start = exclude.End + 1
+				} else if exclude.End >= include.End {
+					// Exclude interval overlaps with the end of include interval
+					include.End = exclude.Start - 1
+				} else {
+					// Exclude interval is in the middle of include interval
+					result = append(result, Interval{Start: include.Start, End: exclude.Start - 1})
+					include.Start = exclude.End + 1
+				}
+			}
+		}
+		if include != (Interval{}) {
+			result = append(result, include)
+		}
+	}
+
+	return result
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func main() {
+	router := mux.NewRouter()
+
+	// Enable CORS
+	headersOk := handlers.AllowedHeaders([]string{"Content-Type"})
+	originsOk := handlers.AllowedOrigins([]string{"*"})
+	methodsOk := handlers.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "OPTIONS"})
+
+	// API endpoint
+	router.HandleFunc("/api/process", handleAPI).Methods("POST")
+
+	// Serve static files for potential frontend (if any)
+	router.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
+
+	// Start the server
+	fmt.Println("Server is running on port 8080...")
+	// Start the server with CORS
+	http.ListenAndServe(":8080", handlers.CORS(originsOk, headersOk, methodsOk)(router))
+}
